b3cd18ea5b8eba1c3a376e3c07bd69fa4552d659
0xDEADBEEF <highimportant@protonmail.com>
2025-02-14 23:26:00 +0300

    migration to esp32-s3

--- include/can_utils.h
+++ include/can_utils.h
@@ -1,14 +1,13 @@
 #ifndef CAN_UTILS_H
 #define CAN_UTILS_H
 
-#include <CAN_config.h>
-#include <ESP32CAN.h>
-
-#include "config.h"
 #include <BleKeyboard.h>
 
+#include "config.h"
+#include "driver/twai.h"  // TWAI (CAN) Driver
+
 // CAN Functions
-void can_setup(CAN_device_t &CAN_cfg);
-void can_receive(CAN_device_t &CAN_cfg, BleKeyboard &bleKeyboard);
+void can_setup();
+void can_receive(BleKeyboard &bleKeyboard);
 
 #endif  // CAN_UTILS_H
